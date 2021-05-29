f7526e7a3de02eb23ecfeead0fafc3f3da6e8437
yasan kahangama <yasanmkahangama@gmail.com>
2021-05-29 14:17:06 +0530

    variable_1

---
+++ variable_1.py
@@ -0,0 +1,11 @@
+
+person_name = "yasan"
+object_1 = "car"
+object_1_color = "red"
+object_name_2 = "duplex"
+object_2_age = "20 years old"
+
+
+print("Hello "+person_name)
+print("I am very interested by your "+object_1+" which is "+object_1_color+" one")
+print("also "+person_name+"! your "+object_name_2+" older than "+object_2_age)
