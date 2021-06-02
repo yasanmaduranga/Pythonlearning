2247f695685b4a76f8742060bc0f6fef10ad4358
yasan kahangama <yasanmkahangama@gmail.com>
2021-06-02 11:01:04 +0530

    number manipulation

---
+++ number_manipulation.py
@@ -0,0 +1,20 @@
+from math import*
+
+print(10+10)
+print(10+10*3)
+print((10+10)*3)
+print(((10+10)*3/98)-32)
+
+age=10
+print("age is "+str(age))
+print(max(1,5,2,7,4,9,-3))
+print(min(1,5,2,7,4,9,-3))
+
+print(pow(5,2))
+print(round(26.54444))
+print(abs(-23))
+print(sqrt(25))
+print(ceil(26.000001))
+print(floor(26.999999))
+print(factorial(5))
+
