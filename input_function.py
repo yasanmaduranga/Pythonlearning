d7870fd0686fe72c7405f9829a53c1d25414ad07
yasan kahangama <yasanmkahangama@gmail.com>
2021-06-02 11:24:12 +0530

    input function

---
+++ input_function.py
@@ -0,0 +1,7 @@
+name=input("what is your name? ")
+age=input("what is your age? ")
+food=input("what do you want to eat? ")
+
+print("hello my name is "+name+" and i am "+age+" years old. Also i like to eat "+food)
+
+#print(age+1)
