6c314962d8316fc337810d001811341630ec1c7a
yasan kahangama <yasanmkahangama@gmail.com>
2021-06-02 10:02:52 +0530

    String_manipulation

---
+++ String_manipulation.py
@@ -0,0 +1,11 @@
+
+sentense = "I love \nPizza"
+
+print(sentense)
+print(sentense.upper())
+print(sentense.lower())
+print(sentense.islower())
+print(len(sentense))
+print(sentense.index('a'))
+print(sentense.replace('Pizza','Cake'))
+print(sentense)
