1c79a9a7eb3103465a46a2cd988e18873e32a50b
yasan kahangama <yasanmkahangama@gmail.com>
2021-06-04 19:42:27 +0530

    function ?

---
+++ function.py
@@ -0,0 +1,18 @@
+def per_1(name):
+    print(name+" hello how can I help You")
+    #print("Hi")
+per_1("Cashier: ")
+
+def person2_1():
+    name = input("what is your name:")
+    food = input("what do you want to eat:")
+    drink = input("what do you want to drink:")
+    dessert = input("what desert do you want:")
+    print(name+" : I would like "+food+" I want to drink "+drink+" I want "+dessert+" as a desset")
+
+person2_1()
+
+def per_1_2(name):
+    print(name+" : thank you")
+
+per_1_2("Yasan")
