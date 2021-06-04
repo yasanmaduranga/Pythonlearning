620799fd9be634e5ad3f1b9a9dfc112ffa77cdc3
yasan kahangama <yasanmkahangama@gmail.com>
2021-06-04 20:24:44 +0530

    if else part 1

---
+++ if_else.py
@@ -0,0 +1,27 @@
+I_want_to_eat= False
+I_want_to_drink = True
+
+# if I_want_to_eat and I_want_to_drink:
+#     print("let have a pizza and drink")
+#     print("ha ha..")
+# else:
+#     print("This is not good time")
+#     print("I don't have money")
+
+# if I_want_to_eat or I_want_to_drink:
+#     print("let have a pizza and drink")
+#     print("ha ha..")
+# else:
+#     print("This is not good time")
+#     print("I don't have money")
+
+if not I_want_to_eat:
+    print("Hi")
+
+# if I_want_to_eat:
+#     print("let have a pizza and drink")
+# elif I_want_to_drink:
+#     print("ha ha..")
+# else:
+#     print("This is not good time")
+ 
