2bad42e2fb5210823b786c3d639a95f46f30c229
yasan kahangama <yasanmkahangama@gmail.com>
2021-06-07 19:45:34 +0530

    dictionnary in python

---
+++ dictionnary.py
@@ -0,0 +1,20 @@
+Names={
+    152:"A",
+    162:"B",
+    172:"C"
+}
+
+print(Names.get(152,"this name do not exist"))
+print(Names.get(162,"this name do not exist"))
+print(Names.get(172,"this name do not exist"))
+print(Names.get(182,"this name do not exist"))
+
+
+Colors={
+    "A":"Red",
+    "B":"Green",
+    "C":"Blue",
+    "D":"Yellow"
+}
+
+print(Colors.get("A","This is not a color"))
