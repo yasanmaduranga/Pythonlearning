834a50b4de63e764044ba4245716e667c6290fbd
yasan kahangama <yasanmkahangama@gmail.com>
2021-06-07 19:30:34 +0530

    touple in python

---
+++ touple.py
@@ -0,0 +1,17 @@
+
+colors=("blue","yellow","red")
+shapes=("square","triangle","circle")
+
+list=colors+shapes
+
+print(list)
+
+#change wenne nathi dewal tama touple wala store krnne
+#ex:- coordinates of paris
+
+#shapes[0]="green"
+
+#there is no insert function in touple
+
+
+print(shapes[0:2])
