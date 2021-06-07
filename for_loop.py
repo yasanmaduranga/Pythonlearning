b8d2d7a7b1971c7a6d8a218f0263b9cdd4ed0a65
yasan kahangama <yasanmkahangama@gmail.com>
2021-06-07 20:30:05 +0530

    for loop in python

---
+++ for_loop.py
@@ -0,0 +1,17 @@
+
+for letter in "blue":
+    print(letter)
+
+
+colors = ["black","yellow","green","red"]
+for one_color in colors:
+    print(one_color)
+    if one_color=="green":
+        break
+
+
+for number in range(5):
+    print(number)
+
+for x in range(20,30):
+    print(x)
