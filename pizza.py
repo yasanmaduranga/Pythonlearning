166da08fe77adf11d037684ab5afe6171f78aad7
yasan kahangama <yasanmkahangama@gmail.com>
2021-06-10 11:29:58 +0530

    classes and objects in python

---
+++ pizza.py
@@ -0,0 +1,6 @@
+class pizza1:
+    def __init__(self,type,price,size):
+        self.type=type
+        self.price=price
+        self.size=size
+        
---
+++ pizza1.py
@@ -0,0 +1,7 @@
+from pizza import pizza1
+
+pizzaY=pizza1("vegetarian",5,"small")
+pizzaP=pizza1("Cheese",10,"large")
+
+print(pizzaY.size)
+print(pizzaP.type)
