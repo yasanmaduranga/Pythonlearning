2f3a9fa550db7db94f2645ad980e6c71a6666447
yasan kahangama <yasanmkahangama@gmail.com>
2021-06-10 11:04:54 +0530

    nested for loops

---
+++ nested_for_loops.py
@@ -0,0 +1,8 @@
+meal=["pizza","burger","Fried rice"]
+drink=["visky","brandy","arrack"]
+desert=["cake","ice-cream","watalappan"]
+
+for m in meal:
+    for d in drink:
+        for de in desert:
+            print("I will have "+m+ " as a meal "+d+" as a drink "+de+" as a dessert")
